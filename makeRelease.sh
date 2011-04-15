9e9ee0e89c792b6a37c39536082ce9a5fefb63ad
James Dunne <james.jdunne@gmail.com>
2011-04-14 22:08:01 -0500

    Release.

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+mkdir LanCaster-Release
 cp -f lcc/bin/Release/lcc.exe* lcc/bin/Release/*.dll lcs/bin/Release/lcs.exe* README LanCaster-Release/
 /cygdrive/c/Program\ Files/7-Zip/7z.exe a LanCaster-Release.zip LanCaster-Release/*
