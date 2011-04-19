f1ba7765292510a95a4cdc16dc001a4c58d97ca7
James Dunne <james.jdunne@gmail.com>
2011-04-19 08:25:48 -0500

    Imported XAML from old project.

---
+++ lcui/Converters.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WellDunne.LanCaster.GUI
+{
+    #region XAML Binding Converters for display
+
+    public class ClientFileCompletionStatusConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return "FIXME";
+#if false
+            ClientFileCompletionStatus status = (ClientFileCompletionStatus)value;
+
+            switch (status)
+            {
+                case ClientFileCompletionStatus.Completed: return "Completed";
+                case ClientFileCompletionStatus.HashMismatch: return "Hash Mismatch";
+                case ClientFileCompletionStatus.InProgress: return "In Progress";
+                case ClientFileCompletionStatus.Missing: return "File Missing";
+                case ClientFileCompletionStatus.NotReceived: return "Not Received";
+                case ClientFileCompletionStatus.Verified: return "Verified";
+                case ClientFileCompletionStatus.Verifying: return "Verifying";
+                case ClientFileCompletionStatus.Partial: return "Partial";
+                default:
+                    return null;
+            }
+#endif
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+    public class FileSizeConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            long size = (long)value;
+
+            if (size >= 1073741824L)
+            {
+                return String.Concat(
+                    (size / 1073741824M).ToString("###,###,###.##"),
+                    " GB"
+                );
+            }
+            else if (size >= 1048576L)
+            {
+                return String.Concat(
+                    (size / 1048576M).ToString("#,###.##"),
+                    " MB"
+                );
+            }
+            else if (size >= 1024L)
+            {
+                return String.Concat(
+                    (size / 1024M).ToString("#,###.##"),
+                    " KB"
+                );
+            }
+            else
+            {
+                return String.Concat(
+                    (size).ToString("#,###.##"),
+                    " bytes"
+                );
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+    public class TransferRateConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            decimal rate = (decimal)value;
+            if (rate >= 1048576)
+            {
+                return String.Concat(
+                    (rate / 1048576.0M).ToString("###,###,##0.00"),
+                    " MB/sec"
+                );
+            }
+            else if (rate >= 1024)
+            {
+                return String.Concat(
+                    (rate / 1024.0M).ToString("###,##0.00"),
+                    " KB/sec"
+                );
+            }
+            else
+            {
+                return String.Concat(
+                    (rate).ToString("##,##0"),
+                    "  B/sec"
+                );
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+    public class ByteArrayBASE64Converter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            byte[] hash = (byte[])value;
+
+            if (hash == null) return "N/A";
+            return System.Convert.ToBase64String(hash, Base64FormattingOptions.None);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ lcui/SectorCompletionBar.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WellDunne.LanCaster.GUI
+{
+    /// <summary>
+    /// Interaction logic for SectorCompletionBar.xaml
+    /// </summary>
+    public partial class SectorCompletionBar : FrameworkElement
+    {
+        public SectorCompletionBar()
+        {
+            this._visual = CreateVisual();
+            this.AddVisualChild(this._visual);
+            this.AddLogicalChild(this._visual);
+        }
+
+        private DrawingVisual _visual;
+        private BitArray _completion = null;
+        private int _currentSector = 0;
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            RenderMyVisual(this._visual);
+            InvalidateVisual();
+        }
+
+        public void UpdateProgress(BitArray completion, int currentSector)
+        {
+            this._completion = completion;
+            this._currentSector = currentSector;
+            RenderMyVisual(this._visual);
+            InvalidateVisual();
+        }
+
+        private DrawingVisual CreateVisual()
+        {
+            var v = new DrawingVisual();
+            RenderMyVisual(v);
+            return v;
+        }
+
+        private void RenderMyVisual(DrawingVisual v)
+        {
+            using (var dc = v.RenderOpen())
+            {
+                RenderRectangles(dc);
+            }
+        }
+
+        private void RenderRectangles(DrawingContext drawingContext)
+        {
+            // Render a vertical gray gradient in a rounded rectangle for the background:
+            GradientStopCollection gsGray = new GradientStopCollection(
+                new GradientStop[3] {
+                    new GradientStop(Colors.LightGray, 0.0d),
+                    new GradientStop(Colors.Silver, 0.5d),
+                    new GradientStop(Colors.DarkGray, 1.0d),
+                }
+            );
+            drawingContext.DrawRoundedRectangle(
+                new LinearGradientBrush(gsGray, 90.0),
+                new Pen(Brushes.Silver, 1.0),
+                new Rect(0.0, 0.0, this.ActualWidth, this.ActualHeight),
+                1.0,
+                1.0
+            );
+
+            BitArray tmp = this._completion;
+            if (tmp == null) return;
+
+            GradientStopCollection gsBlue = new GradientStopCollection(
+                new GradientStop[3] {
+                    new GradientStop(Colors.LightBlue, 0.0d),
+                    new GradientStop(Color.FromRgb(48, 48, 240), 0.5d),
+                    new GradientStop(Colors.DarkBlue, 1.0d),
+                }
+            );
+            Brush blueGradBrush = new LinearGradientBrush(gsBlue, 90.0);
+            Pen bluePen = new Pen(blueGradBrush, 1.0d);
+
+            // Determine how wide each sector should be represented as:
+            double secWidth = this.ActualWidth / (double)_completion.Length;
+            double top = this.ActualHeight * 0.05;
+            double height = this.ActualHeight * 0.9;
+
+            bool ones = false;
+            int starti = 0;
+
+            for (int i = 0; i < tmp.Length; ++i)
+            {
+                if (!ones)
+                {
+                    if (!tmp[i]) continue;
+
+                    starti = i;
+                    ones = true;
+                }
+                else
+                {
+                    if (tmp[i]) continue;
+
+                    ones = false;
+                    // Draw a blue sector rectangle for all non-completed sectors up to here:
+                    drawingContext.DrawRoundedRectangle(blueGradBrush, bluePen, new Rect(starti * secWidth, top, (i - starti) * secWidth, height), 1.0, 1.0);
+                }
+            }
+
+            // Last remaining sector range:
+            if (ones)
+            {
+                // Draw a blue sector rectangle for all non-completed sectors up to here:
+                drawingContext.DrawRoundedRectangle(blueGradBrush, bluePen, new Rect(starti * secWidth, top, (tmp.Length - starti) * secWidth, height), 1.0, 1.0);
+            }
+
+            // Only draw the current sector selector if applicable:
+            if ((this._currentSector >= 0) && (this._currentSector < tmp.Length))
+            {
+                // Draw the current sector pointer as a rounded rectangle around the sector:
+                drawingContext.DrawRoundedRectangle(
+                    null,
+                    new Pen(Brushes.Red, 2.0d),
+                    new Rect(this._currentSector * secWidth, 0.0d, secWidth, this.ActualHeight),
+                    1.0,
+                    1.0
+                );
+            }
+        }
+
+        protected override int VisualChildrenCount { get { return 1; } }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return this._visual;
+        }
+    }
+}
